4737b602496d80bba136accdb436bec150589c55
Shinya Kamiaka <shinya@kamiaka.net>
2021-05-28 11:14:30 +0900

    Add Values type and util functions

--- value.go
+++ value.go
@@ -16,6 +16,8 @@ type Value struct {
 	Value string
 }
 
+type Values []*Value
+
 // WithLabel sets the label `l` and returns it receiver.
 func (v *Value) WithLabel(l string) *Value {
 	v.Label = l
@@ -35,6 +37,11 @@ func String(l, v string) *Value {
 	}
 }
 
+// String appends string Value and return Values.
+func (ls Values) String(l, v string) Values {
+	return append(ls, String(l, v))
+}
+
 // Stringer returns Value.
 func Stringer(l string, v interface{ String() string }) *Value {
 	return &Value{
@@ -43,12 +50,22 @@ func Stringer(l string, v interface{ String() string }) *Value {
 	}
 }
 
+// Stringer appends stringer Value and return Values.
+func (ls Values) Stringer(l string, v interface{ String() string }) Values {
+	return append(ls, Stringer(l, v))
+}
+
 // Stringf returns formatted string Value.
-func Stringf(l string, v string, args ...interface{}) *Value {
+func Stringf(l string, format string, args ...interface{}) *Value {
 	return &Value{
 		Label: l,
-		Value: fmt.Sprintf(v, args...),
+		Value: fmt.Sprintf(format, args...),
 	}
+}
+
+// Stringf appends formatted string Value and return Values.
+func (ls Values) Stringf(l string, format string, args ...interface{}) Values {
+	return append(ls, Stringf(l, format, args...))
 }
 
 // Bool returns Value.
@@ -63,6 +80,11 @@ func Bool(l string, v bool) *Value {
 		Label: l,
 		Value: s,
 	}
+}
+
+// Bool appends bool Value and return Values.
+func (ls Values) Bool(l string, v bool) Values {
+	return append(ls, Bool(l, v))
 }
 
 var hexPrefix = []byte("0x")
@@ -80,12 +102,22 @@ func Bytes(l string, v []byte) *Value {
 	}
 }
 
+// Bytes appends bytes Value and return Values.
+func (ls Values) Bytes(l string, v []byte) Values {
+	return append(ls, Bytes(l, v))
+}
+
 // Byte returns Value.
 func Byte(l string, b byte) *Value {
 	return &Value{
 		Label: l,
 		Value: string(append(hexPrefix, digits[b/16], digits[b%16])),
 	}
+}
+
+// Byte appends byte Value and return Values.
+func (ls Values) Byte(l string, v byte) Values {
+	return append(ls, Byte(l, v))
 }
 
 // Int returns Value.
@@ -96,12 +128,22 @@ func Int(l string, v int) *Value {
 	}
 }
 
+// Int appends int Value and return Values.
+func (ls Values) Int(l string, v int) Values {
+	return append(ls, Int(l, v))
+}
+
 // Int8 returns Value.
 func Int8(l string, v int8) *Value {
 	return &Value{
 		Label: l,
 		Value: strconv.FormatInt(int64(v), 10),
 	}
+}
+
+// Int8 appends Int8 Value and return Values.
+func (ls Values) Int8(l string, v int8) Values {
+	return append(ls, Int8(l, v))
 }
 
 // Int16 returns Value.
@@ -112,12 +154,22 @@ func Int16(l string, v int16) *Value {
 	}
 }
 
+// Int16 appends Int16 Value and return Values.
+func (ls Values) Int16(l string, v int16) Values {
+	return append(ls, Int16(l, v))
+}
+
 // Int32 returns Value.
 func Int32(l string, v int32) *Value {
 	return &Value{
 		Label: l,
 		Value: strconv.FormatInt(int64(v), 10),
 	}
+}
+
+// Int32 appends Int32 Value and return Values.
+func (ls Values) Int32(l string, v int32) Values {
+	return append(ls, Int32(l, v))
 }
 
 // Int64 returns Value.
@@ -128,12 +180,22 @@ func Int64(l string, v int64) *Value {
 	}
 }
 
+// Int64 appends Int64 Value and return Values.
+func (ls Values) Int64(l string, v int64) Values {
+	return append(ls, Int64(l, v))
+}
+
 // Uint returns Value.
 func Uint(l string, v uint) *Value {
 	return &Value{
 		Label: l,
 		Value: strconv.FormatUint(uint64(v), 10),
 	}
+}
+
+// Uint appends Uint Value and return Values.
+func (ls Values) Uint(l string, v uint) Values {
+	return append(ls, Uint(l, v))
 }
 
 // Uint8 returns Value.
@@ -144,12 +206,22 @@ func Uint8(l string, v uint8) *Value {
 	}
 }
 
+// Uint8 appends Uint8 Value and return Values.
+func (ls Values) Uint8(l string, v uint8) Values {
+	return append(ls, Uint8(l, v))
+}
+
 // Uint16 returns Value.
 func Uint16(l string, v uint16) *Value {
 	return &Value{
 		Label: l,
 		Value: strconv.FormatUint(uint64(v), 10),
 	}
+}
+
+// Uint16 appends Uint16 Value and return Values.
+func (ls Values) Uint16(l string, v uint16) Values {
+	return append(ls, Uint16(l, v))
 }
 
 // Uint32 returns Value.
@@ -160,12 +232,22 @@ func Uint32(l string, v uint32) *Value {
 	}
 }
 
+// Uint32 appends Uint32 Value and return Values.
+func (ls Values) Uint32(l string, v uint32) Values {
+	return append(ls, Uint32(l, v))
+}
+
 // Uint64 returns Value.
 func Uint64(l string, v uint64) *Value {
 	return &Value{
 		Label: l,
 		Value: strconv.FormatUint(uint64(v), 10),
 	}
+}
+
+// Uint64 appends Uint64 Value and return Values.
+func (ls Values) Uint64(l string, v uint64) Values {
+	return append(ls, Uint64(l, v))
 }
 
 // Float32 returns Value of float32.
@@ -176,12 +258,22 @@ func Float32(l string, v float32) *Value {
 	}
 }
 
+// Float32 appends Float32 Value and return Values.
+func (ls Values) Float32(l string, v float32) Values {
+	return append(ls, Float32(l, v))
+}
+
 // Float64 returns Value of float64.
 func Float64(l string, v float64) *Value {
 	return &Value{
 		Label: l,
 		Value: fmt.Sprint(v),
 	}
+}
+
+// Float64 appends Float64 Value and return Values.
+func (ls Values) Float64(l string, v float64) Values {
+	return append(ls, Float64(l, v))
 }
 
 // Time returns Value of time.
@@ -192,9 +284,19 @@ func Time(l string, v time.Time) *Value {
 	}
 }
 
+// Time appends Time Value and return Values.
+func (ls Values) Time(l string, v time.Time) Values {
+	return append(ls, Time(l, v))
+}
+
 // UTCTime returns Value of UTC time.
 func UTCTime(l string, v time.Time) *Value {
 	return Time(l, v.UTC())
+}
+
+// UTCTime appends UTCTime Value and return Values.
+func (ls Values) UTCTime(l string, v time.Time) Values {
+	return append(ls, UTCTime(l, v))
 }
 
 // DefaultStackDepth is the depth used when call Stack.
@@ -209,10 +311,20 @@ func Stack(skip int) *Value {
 	}
 }
 
+// Stack appends Stack Value and return Values.
+func (ls Values) Stack(skip int) Values {
+	return append(ls, Stack(skip))
+}
+
 // StackN returns Value of stack trace for N layers.
 func StackN(depth, skip int) *Value {
 	return &Value{
 		Label: "stack",
 		Value: stack.Callers(depth, skip+1).String(),
 	}
+}
+
+// StackN appends Stack Value and return Values.
+func (ls Values) StackN(depth, skip int) Values {
+	return append(ls, StackN(depth, skip+1))
 }
