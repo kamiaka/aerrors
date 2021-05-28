f5a27366a64838db5b21c5fb3bddc520809a97e9
Shinya Kamiaka <shinya@kamiaka.net>
2021-05-28 11:48:03 +0900

    Fix Config type and add util functions

--- option.go
+++ option.go
@@ -6,31 +6,27 @@ type Option func(*Config) *Config
 // Priority option configures error priority.
 func Priority(p ErrorPriority) Option {
 	return func(c *Config) *Config {
-		c.Priority = p
-		return c
+		return c.WithPriority(p)
 	}
 }
 
 // CallerDepth option configures callers depth.
 func CallerDepth(n int) Option {
 	return func(c *Config) *Config {
-		c.CallerDepth = n
-		return c
+		return c.WithCallerDepth(n)
 	}
 }
 
 // CallerSkip option configures callers skip.
 func CallerSkip(n int) Option {
 	return func(c *Config) *Config {
-		c.CallerSkip = n
-		return c
+		return c.WithCallerSkip(n)
 	}
 }
 
 // Formatter option configures error formatter.
 func Formatter(f ErrorFormatter) Option {
 	return func(c *Config) *Config {
-		c.FormatError = f
-		return c
+		return c.WithFormatter(f)
 	}
 }
